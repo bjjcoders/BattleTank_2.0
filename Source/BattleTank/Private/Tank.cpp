e7790c05a4b2640777d2dd0835126f2ab8140d87
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-04-01 17:52:08 -0400

    Projectile Spawns

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -4,6 +4,7 @@
 #include "TankAimingComponent.h"
 #include "TankBarrel.h"
 #include "TankTurret.h"
+#include "Projectile.h"
 
 
 // Sets default values
@@ -19,6 +20,7 @@ ATank::ATank()
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
@@ -51,5 +53,14 @@ void ATank::Fire()
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f Tank Firing"), Time);
+
+	if (!Barrel) { return; }
+
+	//Spawn a projectile at the socket location on the barrel
+	GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+		);
 }
 
