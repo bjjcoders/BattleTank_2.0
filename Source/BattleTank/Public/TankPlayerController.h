30240dc16e40b016be5886a7a29d048d700d8844
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-04-01 12:12:20 -0400

    LookDirection

--- Source/BattleTank/Public/TankPlayerController.h
+++ Source/BattleTank/Public/TankPlayerController.h
@@ -28,4 +28,12 @@ public:
 
 private:
 	bool GetSightRayHitLocation(FVector &HitLocation) const;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector &LookDirection) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.333333;
 };
