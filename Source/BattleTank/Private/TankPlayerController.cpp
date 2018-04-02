d3b22c8ff1a00be696e055d3905a4864465d1d5a
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-04-01 21:41:55 -0400

    Intend Move Forward!!!  No crash!

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -7,16 +7,6 @@ void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Playcontroller not possesing a tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Playcontroller possesing: %s"), *ControlledTank->GetName());
-	}
-
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
