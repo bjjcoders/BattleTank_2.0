34e714dd31523e2577e905d868bd771af6324795
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-04-09 22:00:32 -0400

    Enum for targeting created

--- Source/BattleTank/Public/TankAimingComponent.h
+++ Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,16 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming state
+UENUM()
+enum class EFiringState :uint8
+{
+Reloading,
+Aiming,
+Locked
+};
+
+
 // Forward Decleration
 class UTankBarrel; 
 class UTankTurret;
@@ -26,11 +36,15 @@ public:
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel * Barrel = nullptr;
-
 	UTankTurret * Turret = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
+
 	
 };
