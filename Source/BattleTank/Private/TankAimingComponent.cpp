7075a3326c79b7b1768893545e7031cc0e0d0773
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-05-19 21:56:16 -0400

    Aiming Improved

--- Source/BattleTank/Private/TankAimingComponent.cpp
+++ Source/BattleTank/Private/TankAimingComponent.cpp
@@ -93,7 +93,14 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	
 
 	Barrel->Elevate(DeltaRotator.Pitch); 
-	Turret->Rotate(DeltaRotator.Yaw);
+	if (FMath::Abs(DeltaRotator.Yaw) < 180 )
+	{
+		Turret->Rotate(DeltaRotator.Yaw);
+	}
+	else
+	{
+		Turret->Rotate(-DeltaRotator.Yaw);
+	}
 }
 
 void UTankAimingComponent::Fire()
